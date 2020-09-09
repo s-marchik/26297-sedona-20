2ac554686b496e9a1c4c0b06b4e3fadd591c30ea
s-marchik <65153265+s-marchik@users.noreply.github.com>
2020-09-10 00:39:28 +0300

    Завершаем вёрстку личного проекта

--- source/js/menu-js.js
+++ source/js/menu-js.js
@@ -2,16 +2,14 @@ var nMain = document.querySelector('.main-nav');
 var nClose = document.querySelector('.main-nav__close');
 var nToggle = document.querySelector('.main-nav__toggle');
 
+nToggle.classList.add('main-nav--active');
+
 nToggle.addEventListener('click', function() {
   nClose.classList.add('main-nav--active');
-  nClose.classList.remove('main-nav--inactive');
-  nToggle.classList.add('main-nav--inactive');
   nToggle.classList.remove('main-nav--active');
 });
 
 nClose.addEventListener('click', function() {
   nClose.classList.remove('main-nav--active');
-  nClose.classList.add('main-nav--inactive');
-  nToggle.classList.remove('main-nav--inactive');
   nToggle.classList.add('main-nav--active');
 });

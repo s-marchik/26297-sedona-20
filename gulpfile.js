5c8af7e32bc82c42eecf6d61eae3622d959e3cbf
s-marchik <65153265+s-marchik@users.noreply.github.com>
2020-09-14 07:40:39 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ const svgstore = require("gulp-svgstore");
 const del =require("del");
 
 // Styles
+
 const styles = () => {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
@@ -27,9 +28,11 @@ const styles = () => {
     .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 };
+
 exports.styles = styles;
 
 // Images
+
 const images = () => {
   return gulp.src("source/img/**/*.{jpg,png,svg}")
     .pipe(imagemin([
@@ -39,26 +42,32 @@ const images = () => {
     ]))
     .pipe(gulp.dest("build/img"));
 };
+
 exports.images = images;
 
 // WebP
+
 const webpImages = () => {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("build/img"));
 };
+
 exports.webpImages = webpImages;
 
 // Sprite
+
 const sprite = () => {
   return gulp.src("source/img/icon-*.svg")
     .pipe(svgstore())
     .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("build/img"));
 };
+
 exports.sprite = sprite;
 
 // Server
+
 const server = (done) => {
   sync.init({
     server: {
@@ -70,9 +79,11 @@ const server = (done) => {
   });
   done();
 }
+
 exports.server = server;
 
 // Copy
+
 const copy = () => {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
@@ -84,32 +95,45 @@ const copy = () => {
   })
   .pipe(gulp.dest("build"));
 };
+
 exports.copy = copy;
 
 // Clean
+
 const clean = () => {
   return del("build");
 };
+
 exports.clean = clean;
 
+
 // Watcher
+
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
   gulp.watch("source/*.html", gulp.series("html"));
   gulp.watch("source/js/*.js", gulp.series("copy"));
   gulp.watch("source/*.html").on("change", sync.reload);
 }
+
 exports.default = gulp.series(styles, server, watcher);
 
 // Html
+
 const html = () => {
   return gulp.src(["source/**/*.html"], { base: "source" })
     .pipe(gulp.dest("build"));
 };
+
 exports.html = html;
 
+
+
 // Build
+
 gulp.task("build", gulp.series(clean, copy, styles, images, webpImages, sprite, html));
 
+
 // Gulp start
+
 gulp.task("start", gulp.series("build", server, watcher));
